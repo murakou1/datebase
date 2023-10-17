544e4c8ce47108e1c4df540e8bca011be1326097
Hasegawa891 <e1b21083@oit.ac.jp>
2023-10-17 16:59:26 +0900

    4-4

--- src/main/java/oit/is/team2/datebase/controller/Sample41Controller.java
+++ src/main/java/oit/is/team2/datebase/controller/Sample41Controller.java
@@ -50,4 +50,17 @@ public class Sample41Controller {
   public String sample43() {
     return "sample43.html";
   }
+
+  @PostMapping("step3")
+  @Transactional
+  public String sample43(@RequestParam String chamberName, ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    Chamber chamber3 = new Chamber();
+    chamber3.setChamberName(chamberName);
+    chamber3.setUserName(loginUser);
+    chamberMapper.insertChamber(chamber3);
+    model.addAttribute("chamber3", chamber3);
+    // System.out.println("ID:" + chamber3.getId());
+    return "sample43.html";
+  }
 }
