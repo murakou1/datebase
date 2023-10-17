7021ab2286a57a5fd7fdf88a4d09bc0f282f634a
murakou1 <e1b21105@oit.ac.jp>
2023-10-17 14:10:19 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/team2/datebase/DatebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team2.datebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
